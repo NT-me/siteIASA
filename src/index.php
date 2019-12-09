690c2ca64e6f25a2fcf45b8bf574630686c7ac93
gnouf1 <jeuxforum26@gmail.com>
2019-12-09 02:23:15 +0100

    login terminé

--- src/index.php
+++ src/index.php
@@ -6,24 +6,24 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>IASA</title>
-  <link rel="icon" href="../img/Favicon.png">
+  <link rel="icon" href="Favicon.png">
   <!--<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.min.css">-->
   <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic">
   <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
   <link rel="stylesheet" href="../style/style.css">
 </head>
 
-<body  background style="background-color: black;">
+<body  background style="background: #194993 url(../img/log_wp.jpg) center;">
   <div id="container">
       <!-- zone de connexion -->
-
       <form action="verification.php" method="POST">
-          <h1>Connexion</h1>
+        <img id="logo" src="../img/iasa.png">
+           <font face="orator std"><h1 style="text-align: center">Connexion</h1></font>
 
-          <label><b>Nom d'utilisateur</b></label>
+          <label><b>Nom de code :</b></label>
           <input type="text" placeholder="Entrer le nom d'utilisateur" name="username" required>
 
-          <label><b>Mot de passe</b></label>
+          <label><b>Nom de famille :</b></label>
           <input type="password" placeholder="Entrer le mot de passe" name="password" required>
 
           <input type="submit" id='submit' value='LOGIN' >
