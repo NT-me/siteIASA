14b0c08b44c69ce361476eb5df524a0d58a1f3c3
gnouf1 <jeuxforum26@gmail.com>
2019-12-11 19:28:18 +0100

    ajout statut page principale

--- src/principale.php
+++ src/principale.php
@@ -56,8 +56,46 @@ include('head.php');
               <tr>
                 <th>Service</th>
                 <td><?php
-                if ($niv == 3){
+                $NS = $_SESSION['service'];
+                if ($niv == 2){
                   echo "N/A";
+                }
+                else{
+                  switch ($NS) {
+                    case '86883657':
+                      $NS = "Informatique";
+                      break;
+                    case '44609807':
+                      $NS = "Defense";
+                      break;
+                    case '91345364':
+                      $NS = "Elimination";
+                      break;
+                    case '22195465':
+                      $NS = "Enquete";
+                      break;
+                    case '78334802':
+                      $NS = "Enquete interne";
+                      break;
+                    case '6350262':
+                      $NS = "Action";
+                      break;
+                    case '1738411':
+                      $NS = "Assistance";
+                      break;
+                    case '17410830':
+                      $NS = "Contre-espionnage";
+                      break;
+                  }
+                  if ($niv == 1) {
+                    if ($sexe == "male"){
+                      $NS = $NS." (Directeur)";
+                    }
+                    else if ($sexe == "female"){
+                      $NS = $NS." (Directrice)";
+                    }
+                  }
+                  echo "$NS";
                 }
 
                 ?></td>
